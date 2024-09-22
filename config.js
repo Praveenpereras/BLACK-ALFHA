b770cdc34c02421692c819a85851d9c3ecb85da5
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-22 12:29:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true",
 };
